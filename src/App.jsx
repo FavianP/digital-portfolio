723a327724db12ee23c76b84d3e478a92b87500b
Favian Perez <142631192+FavianP@users.noreply.github.com>
2025-05-18 05:17:32 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {Navbar} from "./components/Navbar";
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home'; 
 import { About } from './components/sections/About';
+import { Project } from './components/sections/Projects';
 function App() {
 const [isLoaded, setIsLoaded] = useState(false);
 const [menuOpen, setMenuOpen] = useState(false);
@@ -19,6 +20,8 @@ const [menuOpen, setMenuOpen] = useState(false);
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <Home />
       <About />
+      <Project />
+      
     </div>
     </> 
   );
